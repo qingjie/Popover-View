82e725dc773f7a89d78eed9305c506b5329a7c49
Merlyn <merlynaivaliotis@Merlyn-Aivaliotiss-iMac-2.local>
2015-03-19 00:04:01 -0700

    fgdfgdfdfg

--- Popover View/PopoverViewController.swift
+++ Popover View/PopoverViewController.swift
@@ -12,9 +12,23 @@ class PopoverViewController: UIViewController {
     
     @IBOutlet weak var videoTitle: UILabel!
     @IBOutlet weak var welcomeMessage: UITextField!
+    
+    var selectedVideoTitle: String!
+    var message: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set the view's backgroundColor to a light mint green color
+        self.view.backgroundColor = UIColor(red:0.93, green:0.98, blue:0.93, alpha:1.00)
+
+        // Set the TextField's textColor, font, and text property
+        welcomeMessage.textColor = UIColor.brownColor()
+        welcomeMessage.font = UIFont.boldSystemFontOfSize(18)
+        welcomeMessage.text = message
+        
+        // Set the Label's text property
+        videoTitle.text = selectedVideoTitle
     }
 
     override func didReceiveMemoryWarning() {
--- Popover View/TableViewController.swift
+++ Popover View/TableViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
-    @IBOutlet weak var selectButton: UIBarButtonItem!
+class TableViewController: UITableViewController, UIPopoverPresentationControllerDelegate {
     var musicVideoList = [String]()
     var cell: UITableViewCell!
+    var selectedMusicVideo = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        musicVideoList = ["Video 1","Video 2","Video 3","Video 4","Video 5"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,17 +36,68 @@ class TableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        // Create a resusable cell
+        // Create a reusable cell
         cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
         
-        // Configure the tableView cell
+        // Configure the reusable cell
+        cell.imageView?.image = UIImage(named: "music-video-32")
+        cell.textLabel?.text = musicVideoList[indexPath.row]
         
+        // Set the selected cell's background to a light mint green color
+        var bgColorView = UIView()
+        bgColorView.backgroundColor = UIColor(red:0.93, green:0.98, blue:0.93, alpha:1.00)
+        cell.selectedBackgroundView = bgColorView
         
         // Return the configured cell
         return cell
     }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let indexPath = self.tableView.indexPathForSelectedRow() {
+            // Put the selected cell's sourceView and sourceRect property value in separate variables
+            let selectedCellSourceView = tableView.cellForRowAtIndexPath(indexPath)
+            let selectedCellSourceRect = cell.bounds
+            
+            // Put the view that'll be shown as a popover in a variable
+            var popover = UIStoryboard(name: "Main",
+                bundle: nil).instantiateViewControllerWithIdentifier("idPopover") as PopoverViewController
+            
+            // Set two control properties of the popover view
+            popover.selectedVideoTitle = musicVideoList[indexPath.row]
+            popover.message = "Welcome back Carter! 🍻🍔🍟"
+            
+            // This statement make the PopoverViewController's view appear as popover on the iOS device
+            popover.modalPresentationStyle = UIModalPresentationStyle.Popover
+            
+            // This statement sets the popover view's anchor arrow's backgroundColor to a light mint green
+            popover.popoverPresentationController?.backgroundColor = UIColor(red:0.93, green:0.98, blue:0.93, alpha:1.00)
+            
+            // Make the popover a delegate of this class
+            popover.popoverPresentationController?.delegate = self
+            
+            // Set the sourceView that activated the popover
+            popover.popoverPresentationController?.sourceView = selectedCellSourceView
+            
+            // Set the area where the popover's anchor arrow will appear
+            popover.popoverPresentationController?.sourceRect = selectedCellSourceRect
+            
+            // Set the direction of the popover's anchor arrow
+            popover.popoverPresentationController?.permittedArrowDirections = .Any
+            
+            // Set the popover view's content width and hight
+            popover.preferredContentSize = CGSizeMake(320, 85)
+            
+            // Show the popoverViewController's view on the device's screen
+            self.presentViewController(popover, animated: true, completion: nil)
+            
+            /***** DEBUG STATEMENTS ****/
+            println("Tapped cell's index number: \(indexPath.row)")
+            println("Selected cell bounds: \(selectedCellSourceRect)")
+            println("Selected music video name:  \(selectedMusicVideo)")
+        }
     }
-    
+
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.None
+    }
 }
